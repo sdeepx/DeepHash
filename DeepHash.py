2a61bf422f1b75b3be671c96d10fb07abf43a4c5
Shubhadeep <41886925+shubha161@users.noreply.github.com>
2019-09-20 23:38:38 +0530

    Add files via upload

--- DeepHash.py
+++ DeepHash.py
@@ -1,98 +1,91 @@
-__author__ = "Shubhadeep"
-__version__ = 1.0
-__company__ = "DeepLab"
-
-'''MD5: Message digest algorithm producing a 128 bit hash value.
- This is widely used to check data integrity.
- It is not suitable for use in other fields due to the security vulnerabilities of MD5.'''
-
-'''SHA: Group of algorithms designed by the U.S's NSA that are part of the U.S Federal Information processing standard. 
-These algorithms are used widely in several cryptographic applications. 
-The message length ranges from 160 bits to 512 bits.'''
-
-print('''
-                                                                    
-    *******     **********  **********  *****      **       **   *****     ******    **       **
-    **    **    **********  **********  **   **    **       **  **   **   **     **  **       **
-    **     **   **          **          **   **    *** * * ***  **   **    **    **  *** * * ***
-    **     **   *******     *******     *****      ***********  *******      **      ***********
-    **     **   **          **          **         *** * * ***  *******  **    **    *** * * ***
-    **    **    **********  **********  **         **       **  **   **  **    **    **       **
-    ******      **********  **********  **         **       **  **   **   ******     **       **
-      
-''')
-
 import hashlib as hl
+from Banner import banner
+from Banner import process
+import time as t
 
-def Hashing(hash):
+
+__author__ = "Shubhadeep"
+__version__ = 1.1
+
+banner.banner_hash()
 
 
-    print('''
-        [+] Choose a Hashing option for Encryption :
-         ______________________________
-                        |
-            1) Md5      |   2) SHA1
-                        |
-            3) SHA224   |   4) SHA256
-                        |
-            5) SHA384   |   6) SHA512
-                        |                
-         _______________|______________
-                  
-              
-    ''')
-    hash_option = input(" : ")
-    lenh = len(hash_option)
-    if lenh == 0:
-        print( " [-] You didn't enter any option")
+def value(inp):
+    len_inp = len(inp)
+    if len_inp == 0:
+        print("[-] Umm! I think you forgot to enter the value")
         return main()
-
-    elif hash_option == "1":
-        print("Please wait .. .. .. .. .. .. .. .. . .. . .. .. . .")
-        print()
-        md5_hash = hl.md5(hash.encode()).hexdigest()
-        return "md5 of {} = {}".format(hash,md5_hash)
-
-    elif hash_option == "2":
-        print("Please wait .. .. .. .. .. .. .. .. . .. . .. .. . .")
-        print()
-        sha1_hash = hl.sha1(hash.encode()).hexdigest()
-        return "sha1 of {} = {}".format(hash,sha1_hash)
-
-    elif hash_option == "3":
-        print("Please wait .. .. .. .. .. .. .. .. . .. . .. .. . .")
-        print()
-        sha224_hash = hl.sha224(hash.encode()).hexdigest()
-        return "sha224 of {} = {}".format(hash,sha224_hash)
-
-    elif hash_option == "4":
-        print("Please wait .. .. .. .. .. .. .. .. . .. . .. .. . .")
-        print()
-        sha256_hash = hl.sha256(hash.encode()).hexdigest()
-        return "sha256 of {} = {}".format(hash,sha256_hash)
-
-    elif hash_option == "5":
-        print("Please wait .. .. .. .. .. .. .. .. . .. . .. .. . .")
-        print()
-        sha384_hash = hl.sha384(hash.encode()).hexdigest()
-        return "sha384 of {} = {}".format(hash, sha384_hash)
-
-    elif hash_option == "6":
-        print("Please wait .. .. .. .. .. .. .. .. . .. . .. .. . .")
-        print()
-        sha512_hash = hl.sha512(hash.encode()).hexdigest()
-        return "sha512 of {} = {}".format(hash, sha512_hash)
-    else:
-        return "[-] You entered a Wrong Value"
+   
 
 def main():
-    print("{+} Enter Your Input For Encryption - ")
-    hash_inp = input(" : ")
-    lenhi = len(hash_inp)
-    if lenhi == 0:
-        print("[-] You didn't enter any Input")
-        return main()
-    else:
-     print(Hashing(hash_inp))
+    print("[+]  Enter the value for Encryption - ")
+    inp = input(" : ")
+    value(inp)
+
+    def options():
+        print('''
+                                            [+] Choose a Hashing option for Encryption :
+                                             ______________________________
+                                                            |
+                                                1) Md5      |   2) SHA1
+                                                            |
+                                                3) SHA224   |   4) SHA256
+                                                            |
+                                                5) SHA384   |   6) SHA512
+                                                            |
+                                             _______________|______________
+
+                ''')
+
+        option = input(" : ")
+        len_opt = len(option)
+        if len_opt == 0:
+            print("[-] Umm! I think you forgot to enter the option")
+            return options()
+        elif option == "1":
+            process.process()
+            md5_hash = hl.md5(inp.encode()).hexdigest()
+            t.sleep(0.5)
+            return "md5 of {} = {}".format(inp, md5_hash)
+        elif option == "2":
+            process.process()
+            print()
+            sha1_hash = hl.sha1(inp.encode()).hexdigest()
+            t.sleep(0.5)
+            return "sha1 of {} = {}".format(inp, sha1_hash)
+        elif option == "3":
+            process.process()
+            print()
+            sha224_hash = hl.sha224(inp.encode()).hexdigest()
+            t.sleep(0.5)
+            return "sha224 of {} = {}".format(inp, sha224_hash)
+        elif option == "4":
+            process.process()
+            print()
+            sha256_hash = hl.sha256(inp.encode()).hexdigest()
+            t.sleep(0.6)
+            return "sha256 of {} = {}".format(inp, sha256_hash)
+        elif option == "5":
+            process.process()
+            print()
+            sha384_hash = hl.sha384(inp.encode()).hexdigest()
+            t.sleep(0.6)
+            return "sha384 of {} = {}".format(inp, sha384_hash)
+        elif option == "6":
+            process.process()
+            print()
+            sha512_hash = hl.sha512(inp.encode()).hexdigest()
+            t.sleep(0.6)
+            return "sha512 of {} = {}".format(inp, sha512_hash)
+        else:
+            print("[-] You chose a wrong value")
+            return options()
+
+    if __name__ == '__main__':
+        process.box_1()
+        print(options())
+        process.box_1()
+
+
 if __name__ == '__main__':
     main()
